4f4438d18fe60138f5cab83dca2968b1fe9438ba
karonyt <tomatotest34@gmail.com>
2025-03-01 13:36:50 +0900

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,10 @@ import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <title>KaroNovel</title>
   <StrictMode>
+    <head>
+      <title>KaroNovel</title>
+    </head>
     <App />
   </StrictMode>
 );
