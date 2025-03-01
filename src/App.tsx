0255bf06d596403f95e5352a49268de2e6571127
karonyt <tomatotest34@gmail.com>
2025-03-01 13:20:46 +0900

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -35,22 +35,6 @@ function App() {
   };
 
   return (
-    <head>
-    <link rel="apple-touch-icon" sizes="57x57" href="https://karonyt.github.io/karonovel/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="https://karonyt.github.io/karonovel/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="https://karonyt.github.io/karonovel/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="https://karonyt.github.io/karonovel/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="https://karonyt.github.io/karonovel/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="https://karonyt.github.io/karonovel/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="https://karonyt.github.io/karonovel/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="https://karonyt.github.io/karonovel/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="https://karonyt.github.io/karonovel/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="https://karonyt.github.io/karonovel/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="https://karonyt.github.io/karonovel/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="https://karonyt.github.io/karonovel/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="https://karonyt.github.io/karonovel/favicon-16x16.png">
-    <link rel="manifest" href="https://karonyt.github.io/karonovel/manifest.json">
-    </head>
     <div className="min-h-screen bg-gray-50 flex flex-col">
       {/* Header */}
       <header className="bg-indigo-700 text-white shadow-md">
